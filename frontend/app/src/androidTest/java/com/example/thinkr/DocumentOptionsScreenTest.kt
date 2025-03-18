0cebc0bce0fb6cd7c97c4dbde06c909a7c2735b4
pjavanrood <Parshan.pjavanrood@gmail.com>
2025-03-17 21:44:41 -0700

    fix: addressing new codacy issues

--- frontend/app/src/androidTest/java/com/example/thinkr/DocumentOptionsScreenTest.kt
+++ frontend/app/src/androidTest/java/com/example/thinkr/DocumentOptionsScreenTest.kt
@@ -23,7 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import java.lang.Thread.sleep
 
-class DocumentOptionsScreenTest {
+internal class DocumentOptionsScreenTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
